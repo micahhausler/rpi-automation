bac8430a391c21eb47a15c5ac4057b97e308b90c
Micah Hausler <hausler.m@gmail.com>
2017-07-08 23:33:40 +0000

    feat(setup): Added Kubernetes apt dependencies
    * Added conntrack and socat

--- setup.d/10-apt.sh
+++ setup.d/10-apt.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# Install development tools
+
 sudo apt-get install -y \
+    conntrack \
     curl \
     git \
     htop \
     jq \
     less \
     man \
+    socat \
     vim \
     wget \
     x11-apps
+
+sudo chown -R pi:pi /usr/local/
