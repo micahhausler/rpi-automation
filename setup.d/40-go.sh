39000561d54652508bc9c90938aef76a49b12a50
Micah Hausler <hausler.m@gmail.com>
2017-07-08 23:29:59 +0000

    refactor(setup): Don't reinstall go
    * Only installs go if desired version differs from installed
    * Look for GO_VERSION environment varabiable

--- setup.d/40-go.sh
+++ setup.d/40-go.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-VERSION="1.8.3"
+# Install the Go language
+
+GO_VERSION="${GO_VERSION:-1.8.3}"
 OS="linux"
 ARCH="armv6l"
 
-if [[ ! -d /usr/local/go ]];
+if [[ ! -d /usr/local/go ]]; then
     mkdir -p /usr/local/go;
 fi
 
-TARBALL="go$VERSION.$OS-$ARCH.tar.gz"
-URL="https://storage.googleapis.com/golang/$TARBALL"
+if [ -f /usr/local/go/bin/go ]; then
+    if [ "$(/usr/local/go/bin/go version)" != "go version go$GO_VERSION linux/arm" ]; then
+        TARBALL="go$GO_VERSION.$OS-$ARCH.tar.gz"
+        URL="https://storage.googleapis.com/golang/$TARBALL"
 
-wget --progress dot:mega -P /tmp $URL
-tar -C /usr/local -xzf /tmp/$TARBALL
+        wget --progress dot:mega -P /tmp $URL
+        tar -C /usr/local -xzf /tmp/$TARBALL
+    fi
+fi
